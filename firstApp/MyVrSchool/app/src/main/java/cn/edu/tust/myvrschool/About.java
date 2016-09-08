ef52862c78b3c8dd65c2e0799dc580d4bc241246
Sirlsliang <648767807@qq.com>
2016-09-08 11:59:15 +0800

    add about dialog

---
+++ firstApp/MyVrSchool/app/src/main/java/cn/edu/tust/myvrschool/About.java
@@ -0,0 +1,15 @@
+package cn.edu.tust.myvrschool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MyPC on 2016/9/8.
+ */
+public class About extends Activity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
--- firstApp/MyVrSchool/app/src/main/java/cn/edu/tust/myvrschool/MainActivity.java
+++ firstApp/MyVrSchool/app/src/main/java/cn/edu/tust/myvrschool/MainActivity.java
@@ -1,13 +1,33 @@
 package cn.edu.tust.myvrschool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set up listeners for all the buttons
+        View continueButton = findViewById(R.id.about_button);
+        continueButton.setOnClickListener(this);
+        View newButton  = findViewById(R.id.new_game_button);
+        newButton.setOnClickListener(this);
+
+
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.about_button:
+                Intent i = new Intent(this,About.class);
+                startActivity(i);
+                break;
+        }
     }
 }
