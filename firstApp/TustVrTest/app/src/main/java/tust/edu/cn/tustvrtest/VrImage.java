75f8f538bc74c29bf9e753e047aea281c86a2051
Sirlsliang <648767807@qq.com>
2016-09-18 21:21:32 +0800

    添加了GVR Demo中的VR视频播放

---
+++ firstApp/TustVrTest/app/src/main/java/tust/edu/cn/tustvrtest/VrImage.java
@@ -0,0 +1,186 @@
+package tust.edu.cn.tustvrtest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaEventListener;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by MyPC on 2016/9/18.
+ */
+public class VrImage extends Activity {
+    private static final String TAG = Main.class.getSimpleName();
+    /*
+    * Arbitrary variable to track load status.
+    *  In this example, this variable should only be accessed on the UI thread.
+    *  In a real app, this variable would be code that performs some UI actions
+    *  when the panorama is fully loaded.
+    * */
+    public boolean loadImageSuccessful;
+    /*
+    * Tracks the file to be loaded across the lifetime of this app
+    * */
+    private Uri fileUri;
+    //Configuration information for the panorama
+    private VrPanoramaView.Options panoOptions = new VrPanoramaView.Options();
+    private ImageLoaderTask backgroundImageLoaderTask;
+    //Actual panorama widget
+    private VrPanoramaView panoWidgetView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vrimage);
+
+        //Make the source link clickable
+        TextView sourceText = (TextView)findViewById(R.id.source);
+        sourceText.setText(Html.fromHtml(getString(R.string.source)));
+        sourceText.setMovementMethod(LinkMovementMethod.getInstance());
+
+        panoWidgetView = (VrPanoramaView) findViewById(R.id.pano_view);
+        panoWidgetView.setEventListener(new ActivityEventListener());
+
+        //Initial launch of the app or an activity recreation due to rotation
+        handleIntent(getIntent());
+
+    }
+    /*Load custom images based on the intent or load the default image
+    See the Javadoc for this class for information on generating a custom intent via adb.
+    * */
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_VIEW.equals(intent.getAction())){
+            Log.i(TAG,"ACTION_VIEW Intent recieved");
+
+            fileUri = intent.getData();
+            if(fileUri == null){
+                Log.w(TAG,"No data uri specified. Use \"-d /path/fileName\"");
+            }else{
+                Log.i(TAG,"Using file " + fileUri.toString());
+            }
+            panoOptions.inputType = intent.getIntExtra("inputType", VrPanoramaView.Options.TYPE_MONO);
+            Log.i(TAG,"Options.inputType = " + panoOptions.inputType);
+        }else{
+            Log.i(TAG,"Intent is not ACTION_VIEW. Using default pano image.");
+            fileUri = null;
+            panoOptions.inputType = VrPanoramaView.Options.TYPE_MONO;
+        }
+        //Load the bitmap in a background thread to avoid blocking the UI thread.
+        // This operation can take 100s of milliseconds
+        if(backgroundImageLoaderTask  != null){
+            //Cancel any task from a previous intent sent to this activity
+            backgroundImageLoaderTask.cancel(true);
+        }
+        backgroundImageLoaderTask = new ImageLoaderTask();
+        backgroundImageLoaderTask.execute(Pair.create(fileUri,panoOptions));
+
+    }
+    @Override
+    protected void onPause() {
+        panoWidgetView.pauseRendering();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        panoWidgetView.resumeRendering();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Destroy the widget and free memory.
+        panoWidgetView.shutdown();
+
+        // The background task has a 5 second timeout so it can potentially stay alive for 5 seconds
+        // after the activity is destroyed unless it is explicitly cancelled.
+        if (backgroundImageLoaderTask != null) {
+            backgroundImageLoaderTask.cancel(true);
+        }
+        super.onDestroy();
+    }
+    /*Listen to the important events from widget
+    * */
+    private class ActivityEventListener extends VrPanoramaEventListener {
+        /*called by pano widget on the UI thread when it is
+        done loading the image
+        * */
+        @Override
+        public void onLoadSuccess(){
+            loadImageSuccessful = true;
+        }
+        /*
+        * Called by pano widget on the UI thread on any asynchronous error
+        * */
+
+        @Override
+        public void onLoadError(String errorMessage){
+            loadImageSuccessful = false;
+            Toast.makeText(VrImage.this,"Error loading pano: "+errorMessage, Toast.LENGTH_LONG).show();
+            Log.e(TAG,"Error loading pano: "+errorMessage);
+        }
+
+    }
+
+    class ImageLoaderTask extends AsyncTask<Pair<Uri,VrPanoramaView.Options>,Void,Boolean> {
+        VrPanoramaView.Options panoOptions = null;  // It's safe to use null VrPanoramaView.Options.
+        InputStream istr = null;
+        /**
+         * Reads the bitmap from disk in the background and waits until it's loaded by pano widget.
+         */
+        @Override
+        protected Boolean doInBackground(Pair<Uri, VrPanoramaView.Options>... fileInformation) {
+
+            if (fileInformation == null || fileInformation.length < 1
+                    || fileInformation[0] == null || fileInformation[0].first == null) {
+                AssetManager assetManager = getAssets();
+                try {
+                    String[] names = assetManager.list("");
+                    istr = assetManager.open("image/2.jpg");
+                    panoOptions = new VrPanoramaView.Options();
+                    panoOptions.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not decode default bitmap: " + e);
+                    return false;
+                }
+            } else {
+                try {
+                    istr = new FileInputStream(new File(fileInformation[0].first.getPath()));
+                    panoOptions = fileInformation[0].second;
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not load file: " + e);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        @Override
+        protected void onPostExecute(Boolean aBoolean){
+            if(istr!=null&&panoOptions!=null){
+                panoWidgetView.loadImageFromBitmap(BitmapFactory.decodeStream(istr), panoOptions);
+                try {
+                    istr.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not close input stream: " + e);
+                }
+            }
+            super.onPostExecute(aBoolean);
+        }
+    }
+}
--- firstApp/TustVrTest/app/src/main/java/tust/edu/cn/tustvrtest/VrVideo.java
+++ firstApp/TustVrTest/app/src/main/java/tust/edu/cn/tustvrtest/VrVideo.java
@@ -1,7 +1,286 @@
 package tust.edu.cn.tustvrtest;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+import com.google.vr.sdk.widgets.video.VrVideoEventListener;
+import com.google.vr.sdk.widgets.video.VrVideoView;
+
+import java.io.IOException;
+
 /**
  * Created by MyPC on 2016/9/17.
  */
-public class VrVideo {
+public class VrVideo extends Activity {
+    private static final  String TAG = VrVideo.class.getSimpleName();
+    /*查看视频加载状态，
+    * */
+    public static final int LOAD_VIDEO_STATUS_UNKNOWN = 0;
+    public static final int LOAD_VIDEO_STATUS_SUCCESS = 1;
+    public static final int LOAD_VIDEO_STATUS_ERROR   = 2;
+    private int loadVideoStatus = LOAD_VIDEO_STATUS_UNKNOWN;
+    //当屏幕旋转时保留视频的状态
+    private static final String STATE_IS_PAUSED = "isPaused";
+    private static final String STATE_PROGRESS_TIME = "progressTime";
+    //视频的长度不需要保留，在本次试验中保留是为了可以让seekBar在onRestoreInstanceState中就可以配置。
+    //而不必等着视频加载完毕后再配置
+    private static final String STATE_VIDEO_DURATION = "videoDuration";
+    /*
+    * video的控制按钮
+    * */
+    private SeekBar seekBar;
+    private TextView statusText;
+    /*video的视图框与其UI元素
+    * */
+    private VrVideoView videoWidgetView;
+
+    //默认情况下视频加载完毕后即可播放，
+    private boolean isPaused = false;
+
+    //Tracks the file to be loaded across the lifetime of this app
+    private Uri fileUri;
+    //视频的配置信息
+    private VrVideoView.Options videoOptions = new VrVideoView.Options();
+    private VideoLoaderTask backgroundVideoLoaderTask;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video);
+
+        seekBar = (SeekBar) findViewById(R.id.video_seek_bar);
+        seekBar.setOnSeekBarChangeListener(new SeekBarListener());
+        statusText = (TextView) findViewById(R.id.video_status_text);
+        //使连接生效
+        TextView sourceText = (TextView) findViewById(R.id.video_source);
+        sourceText.setText(Html.fromHtml(getString(R.string.video_source)));
+        sourceText.setMovementMethod(LinkMovementMethod.getInstance());
+        //Bind input and output objects for the view
+        videoWidgetView = (VrVideoView)findViewById(R.id.video_view);
+        videoWidgetView.setEventListener(new ActivityEventListener());
+
+        loadVideoStatus = LOAD_VIDEO_STATUS_UNKNOWN;
+        /*
+        *处理初始化app的登录或因旋转造成的recreation
+         */
+        handleIntent(getIntent());
+
+
+    }
+    //当Activity已经处于Running状态并赋予一个新的intent时使用
+    @Override
+    protected  void onNewIntent(Intent intent){
+        Log.i(TAG,this.hashCode() + ".onNewIntent()");
+        /*保存Intent，这允许在onCreate()方法中调用的getIntent()获取该新的Intent，并在以后的使用
+        * */
+        setIntent(intent);
+        //load the new image
+        handleIntent(intent);
+    }
+    /*
+    *
+    * */
+    private void handleIntent(Intent intent){
+        //判断Intent是否需要加载文件
+        if(Intent.ACTION_VIEW.equals(intent.getAction())){
+            Log.i(TAG,"ACTION_VIEW Intent received");
+            fileUri = intent.getData();
+            if(fileUri == null){
+                Log.w(TAG,"No data uri specified. Use \" -d path/filename\" .");
+            }else{
+                Log.i(TAG,"Using file " + fileUri.toString());
+            }
+            videoOptions.inputFormat = intent.getIntExtra("inputFormat", VrVideoView.Options.FORMAT_DEFAULT);
+            videoOptions.inputType = intent.getIntExtra("inputType", VrVideoView.Options.TYPE_MONO);
+        }else{
+            Log.i(TAG,"Intent is not ACTION_VIEW. Using the default video");
+            fileUri = null;
+        }
+        //利用后台线程加载位图，以避免阻塞线程。
+        if(backgroundVideoLoaderTask !=null){
+            backgroundVideoLoaderTask.cancel(true);
+        }
+        backgroundVideoLoaderTask = new VideoLoaderTask();
+        backgroundVideoLoaderTask.execute(Pair.create(fileUri,videoOptions));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong(STATE_PROGRESS_TIME,videoWidgetView.getCurrentPosition());
+        savedInstanceState.putLong(STATE_VIDEO_DURATION,videoWidgetView.getDuration());
+        savedInstanceState.putBoolean(STATE_IS_PAUSED,isPaused);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        long progressTime = savedInstanceState.getLong(STATE_PROGRESS_TIME);
+        videoWidgetView.seekTo(progressTime);
+        seekBar.setMax((int)savedInstanceState.getLong(STATE_VIDEO_DURATION));
+        seekBar.setProgress((int)progressTime);
+        isPaused = savedInstanceState.getBoolean(STATE_IS_PAUSED);
+        if(isPaused)
+            videoWidgetView.pauseVideo();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //阻止view继续在后台渲染
+        videoWidgetView.pauseRendering();;
+        //如果视频正在播放时调用onPause()，默认将会暂停直到调用onResume方法
+        isPaused = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //恢复3D渲染
+        videoWidgetView.resumeRendering();
+
+        updateStatusText();
+    }
+
+    @Override
+    protected void onDestroy() {
+        videoWidgetView.shutdown();
+        super.onDestroy();
+
+    }
+
+    private void updateStatusText(){
+        StringBuilder status = new StringBuilder();
+        status.append(isPaused?"Paused: ":"Playing: ");
+        status.append(String.format("%.2f",videoWidgetView.getCurrentPosition()/1000f));
+        status.append("/");
+        status.append(videoWidgetView.getDuration()/1000f);
+        status.append("seconds");
+        statusText.setText(status.toString());
+
+    }
+    private void togglePause(){
+        if(isPaused){
+            videoWidgetView.playVideo();
+        }else{
+            videoWidgetView.pauseVideo();
+        }
+        isPaused = !isPaused;
+        updateStatusText();
+    }
+    //用户控制seekbar时，修改视频的位置
+    private class SeekBarListener implements SeekBar.OnSeekBarChangeListener {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if(fromUser){
+                videoWidgetView.seekTo(progress);
+                updateStatusText();
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+    //监听来自视频播放框的事件
+    private class ActivityEventListener extends VrVideoEventListener{
+        //视频加载完毕后，调用该方法
+        @Override
+        public void onLoadSuccess() {
+            Log.i(TAG,"Successfully loaded video" + videoWidgetView.getDuration());
+            super.onLoadSuccess();
+        }
+        //视频加载出错时调用
+        @Override
+        public void onLoadError(String errorMessage) {
+            //基本上是因为无法解码视频格式的错误
+            loadVideoStatus = LOAD_VIDEO_STATUS_ERROR;
+            Toast.makeText(VrVideo.this,"Error loading video"+errorMessage,Toast.LENGTH_LONG).show();
+            Log.e(TAG,"Error loading video: "+errorMessage);
+        }
+
+        @Override
+        public void onClick() {
+            //super.onClick();
+            togglePause();
+        }
+        //每帧调用该方法更新UI
+        @Override
+        public void onNewFrame(){
+            updateStatusText();
+            seekBar.setProgress((int)videoWidgetView.getCurrentPosition());
+        }
+        //使视频循环播放。也可以利用该方法切换视频
+        @Override
+        public void onCompletion() {
+            videoWidgetView.seekTo(0);
+            //super.onCompletion();
+
+
+        }
+    }
+
+    //Helper class to manage threading
+    class VideoLoaderTask extends AsyncTask<Pair<Uri,VrVideoView.Options>,Void,Boolean>{
+        private VrVideoView.Options options;
+        private boolean bloadFromAssets = false;
+        private Pair<Uri,VrVideoView.Options> infor;
+
+        @Override
+        protected Boolean doInBackground(Pair<Uri, VrVideoView.Options>... fileInformation) {
+
+            if(fileInformation == null || fileInformation.length<1
+                    ||fileInformation[0] == null || fileInformation[0].first == null){
+                options = new VrVideoView.Options();
+                options.inputType = VrVideoView.Options.TYPE_STEREO_OVER_UNDER;
+                bloadFromAssets = true;
+            }else{
+                bloadFromAssets = false;
+               infor = fileInformation[0];
+            }
+
+            return true;
+        }
+
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            try{
+                if(bloadFromAssets) {
+                    videoWidgetView.loadVideoFromAsset("video/congo.mp4", options);
+                }else{
+                    videoWidgetView.loadVideo(infor.first,infor.second);
+                }
+            }catch (IOException e){
+                loadVideoStatus = LOAD_VIDEO_STATUS_ERROR;
+                videoWidgetView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(VrVideo.this,"Error opening file.",Toast.LENGTH_LONG).show();
+                    }
+                });
+                Log.e(TAG,"Could not open video"+e);
+            }
+
+        }
+    }
+
 }
